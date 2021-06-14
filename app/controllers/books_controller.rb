df7fd2d6ba9c8ed8caa7b547b95acfff0dab7f85
tomoyukikubota <tomoyukikubota0119@gmail.com>
2021-06-14 02:49:57 +0000

    [Finish]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,33 +1,41 @@
 class BooksController < ApplicationController
+  
+  def top
+  end
+  
   def show
     @book = Book.find(params[:id])
   end
 
   def index
     @books = Book.all
-  end
-
-  def new
     @book = Book.new
   end
-
+  
   def create
-    book = Book.new(book_params)
-    if book.save
-      flash[:notice] = 'Book was successfully created.'
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+    flash[:notice] = 'Book was successfully created.'
+    redirect_to book_path(@book.id)
+    else
+    @books = Book.all
+    render :index
     end
   end
+
 
   def edit
     @book = Book.find(params[:id])
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-      flash[:notice] = 'Book was successfully updated.'
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = 'Book was successfully updated.'
+    redirect_to book_path(@book)
+    else
+    @books = Book.all
+    render :edit
     end
   end
 
